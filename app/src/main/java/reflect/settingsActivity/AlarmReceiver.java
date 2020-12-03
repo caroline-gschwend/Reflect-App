ca04d4a1b9b30d623397913f7c9a2a656c625d96
grdirkso <36120935+grdirkso@users.noreply.github.com>
2020-12-03 10:50:43 -0600

    originally working alarm

--- app/src/main/java/reflect/settingsActivity/AlarmReceiver.java
+++ app/src/main/java/reflect/settingsActivity/AlarmReceiver.java
@@ -7,11 +7,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
-import android.util.Log;
-import android.widget.Toast;
-
 import androidx.annotation.RequiresApi;
-import androidx.core.app.NotificationBuilderWithBuilderAccessor;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.TaskStackBuilder;
 
@@ -21,39 +17,39 @@ import reflect.todolistactivity.MoodEntryListActivity;
 
 public class AlarmReceiver extends BroadcastReceiver {
 
-    String ALARM_CHANNEL_ID = "alarm_channel";
-    @RequiresApi(api = Build.VERSION_CODES.O)
-    @Override
-    public void onReceive(Context context, Intent intent){
-        System.out.println("hit");
-        int requestId = 0;
-        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        if(intent.hasExtra("RequestID")){
-            requestId = intent.getIntExtra("RequestID", 0);
+        String ALARM_CHANNEL_ID = "alarm_channel";
+
+        @RequiresApi(api = Build.VERSION_CODES.O)
+        @Override
+        public void onReceive(Context context, Intent intent) {
+
+            System.out.println("hit");
+            int requestId = 0;
+            NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            NotificationCompat.Builder alarm_builder = new NotificationCompat.Builder(context, ALARM_CHANNEL_ID)
+                    .setSmallIcon(R.drawable.ic_launcher_background)
+                    .setContentTitle("Mood Entry Time!")
+                    .setContentText("It's time to enter your " + intent.getStringExtra("time") + " mood")
+                    .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+                    .setChannelId(ALARM_CHANNEL_ID);
+            Intent resultIntent = new Intent(context, MoodEntryListActivity.class);
+            TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+            stackBuilder.addParentStack(SettingsActivity.class);
+            stackBuilder.addNextIntent(resultIntent);
+            PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(requestId, PendingIntent.FLAG_UPDATE_CURRENT);
+            alarm_builder.setContentIntent(resultPendingIntent);
+
+
+            String channelId = ALARM_CHANNEL_ID;
+            NotificationChannel channel = new NotificationChannel(
+                    channelId,
+                    "AlarmNotifications",
+                    NotificationManager.IMPORTANCE_HIGH);
+            notificationManager.createNotificationChannel(channel);
+            channel.enableVibration(true);
+
+            notificationManager.notify(requestId, alarm_builder.build());
         }
-        NotificationCompat.Builder alarm_builder = new NotificationCompat.Builder(context, ALARM_CHANNEL_ID)
-                .setSmallIcon(R.drawable.ic_launcher_background)
-                .setContentTitle("ToDo Item due now!")
-                .setContentText("The due date for item '" + intent.getStringExtra("title") + "' is " + intent.getStringExtra("dueDate"))
-                .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
-                .setChannelId(ALARM_CHANNEL_ID);
-        Intent resultIntent = new Intent(context, MoodEntryListActivity.class);
-        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
-        stackBuilder.addParentStack(SettingsActivity.class);
-        stackBuilder.addNextIntent(resultIntent);
-        PendingIntent resultPendingIntent = stackBuilder.getPendingIntent(requestId, PendingIntent.FLAG_UPDATE_CURRENT);
-        alarm_builder.setContentIntent(resultPendingIntent);
 
-
-        String channelId = ALARM_CHANNEL_ID;
-        NotificationChannel channel = new NotificationChannel(
-                channelId,
-                "AlarmNotifications",
-                NotificationManager.IMPORTANCE_HIGH);
-        notificationManager.createNotificationChannel(channel);
-        channel.enableVibration(true);
-
-        notificationManager.notify(requestId,alarm_builder.build());
-    }
     }
 
--- app/src/main/java/reflect/settingsActivity/SettingsActivity.java
+++ app/src/main/java/reflect/settingsActivity/SettingsActivity.java
@@ -2,6 +2,7 @@ package reflect.settingsActivity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.app.AlarmManager;
 import android.app.DatePickerDialog;
 import android.app.PendingIntent;
@@ -16,6 +17,7 @@ import android.text.format.DateFormat;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.DatePicker;
 import android.widget.Switch;
 import android.widget.TextView;
@@ -29,52 +31,56 @@ import reflect.todolistactivity.MoodEntryListActivity;
 
 public class SettingsActivity extends AppCompatActivity {
 
-    private static Calendar calendar;
-    private TimePickerDialog.OnTimeSetListener onSetTime;
-    private AlarmManager alarmMgr;
-    private PendingIntent alarmIntent;
-    private int alarmTime;
-    private View thisView;
-    private TextView morningTime;
 
+    private static Calendar calendar;
+    private DatePickerDialog.OnDateSetListener onSetDate;
+    private TimePickerDialog.OnTimeSetListener onSetTime;
+    private TextView morningTime;
+    private TextView afternoonTime;
+    private TextView eveningTime;
+    private String timeOfDay;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
         calendar = Calendar.getInstance();
 
-        Button setMorning = findViewById(R.id.morningBtn);
+        final Button setMorning = findViewById(R.id.morningBtn);
+        final Button setAfternoon = findViewById(R.id.afternoonBtn);
+        final Button setEvening = findViewById(R.id.eveningBtn);
         Button done = findViewById(R.id.doneBtn);
         morningTime = findViewById(R.id.morningTv);
-        Switch morningSwitch = findViewById(R.id.mSwitch);
-
+        final Switch morningSwitch = findViewById(R.id.mSwitch);
+        afternoonTime = findViewById(R.id.afternoonTv);
+        final Switch afternoonSwitch = findViewById(R.id.aSwitch);
+        eveningTime = findViewById(R.id.eveningTv);
+        final Switch eveningSwitch = findViewById(R.id.nSwitch);
 
         done.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 AlarmManager alarmManager;
                 alarmManager = (AlarmManager) view.getContext().getSystemService(Context.ALARM_SERVICE);
-                Intent intent = new Intent(view.getContext(), AlarmReceiver.class);
-                final int notificationId = 0;
-                intent.putExtra("RequestID", notificationId);
-                PendingIntent dueDateIntent = PendingIntent.getBroadcast(view.getContext(), 0, intent, 0);
-                alarmManager.setExact(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), dueDateIntent);
+                System.out.println(calendar.getTime());
+                Intent dueDateNotificationIntent = new Intent(view.getContext(), AlarmReceiver.class);
+                dueDateNotificationIntent.putExtra("time", "morning");
+                PendingIntent dueDateIntent = PendingIntent.getBroadcast(view.getContext(), 0, dueDateNotificationIntent, 0);
+                alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, dueDateIntent);
+                
+
+                //end
                 finish();
+
             }
         });
-        /**
-         * Method for when date/time is to be set
-         * will show the date and time pickers
-         */
+
         setMorning.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Calendar calendar = Calendar.getInstance();
-
                 int minute = calendar.get(Calendar.MINUTE);
                 int hour = calendar.get(Calendar.HOUR);
-
 
                 TimePickerDialog timePickerDialog = new TimePickerDialog(
                         SettingsActivity.this,
@@ -95,13 +101,106 @@ public class SettingsActivity extends AppCompatActivity {
 
                 Log.d("AddEditToDoItem", "onTimeSet: hh:mm " + hour + ":" + minute);
 
+                System.out.println(calendar.get(Calendar.DAY_OF_MONTH));
                 calendar.set(Calendar.MINUTE, minute);
                 calendar.set(Calendar.HOUR, hour);
-                morningTime.setText(calendar.getTime().toString());
+                calendar.set(Calendar.DAY_OF_MONTH, calendar.get(Calendar.DAY_OF_MONTH));
+                morningTime.setText(hour + ":" + minute);
             }
         };
 
     }
 
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_settings);
+//        calendar = Calendar.getInstance();
+//
+//        final Button setMorning = findViewById(R.id.morningBtn);
+//        final Button setAfternoon = findViewById(R.id.afternoonBtn);
+//        final Button setEvening = findViewById(R.id.eveningBtn);
+//        Button done = findViewById(R.id.doneBtn);
+//        morningTime = findViewById(R.id.morningTv);
+//        final Switch morningSwitch = findViewById(R.id.mSwitch);
+//        afternoonTime = findViewById(R.id.afternoonTv);
+//        final Switch afternoonSwitch = findViewById(R.id.aSwitch);
+//        eveningTime = findViewById(R.id.eveningTv);
+//        final Switch eveningSwitch = findViewById(R.id.nSwitch);
+//
+//
+//
+//        done.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                AlarmManager alarmManager = (AlarmManager) view.getContext().getSystemService(Context.ALARM_SERVICE);
+//                //create alarm and notification
+//                Intent dueDateNotificationIntent = new Intent(view.getContext(), AlarmReceiver.class);
+//                dueDateNotificationIntent.putExtra("time", "morning");
+//                PendingIntent dueDateIntent = PendingIntent.getBroadcast(view.getContext(), 0, dueDateNotificationIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+//                System.out.println(calendar.getTime());
+//                alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), 0, dueDateIntent);
+//                finish();
+//            }
+//
+//
+//        });
+//
+//        /**
+//         * Method for when morning time is to be set
+//         * will show the date and time pickers
+//         */
+//        setMorning.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Calendar calendar = Calendar.getInstance();
+//                int minute = calendar.get(Calendar.MINUTE);
+//                int hour = calendar.get(Calendar.HOUR);
+//
+//                TimePickerDialog timePickerDialog = new TimePickerDialog(
+//                        SettingsActivity.this,
+//                        onSetTime,
+//                        minute, hour, DateFormat.is24HourFormat(SettingsActivity.this));
+//                timePickerDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.WHITE));
+//                timePickerDialog.show();
+//
+//            }
+//        });
+//
+//
+//        /**
+//         * Method to set the time
+//         * will set the time from the hour and minute of the time picker
+//         */
+//        onSetTime = new TimePickerDialog.OnTimeSetListener() {
+//            @Override
+//            public void onTimeSet(TimePicker timePicker, int hour, int minute) {
+//
+//                Log.d("AddEditToDoItem", "onTimeSet: hh:mm " + hour + ":" + minute);
+//
+//                    calendar.set(Calendar.MINUTE, minute);
+//                    calendar.set(Calendar.HOUR, hour);
+//                    calendar.set(Calendar.YEAR, calendar.get(Calendar.YEAR));
+//                    calendar.set(Calendar.MONTH, calendar.get(Calendar.MONTH));
+//                    calendar.set(Calendar.DAY_OF_MONTH, calendar.get(Calendar.DAY_OF_MONTH) - 1);
+//                    morningTime.setText(hour + ":" + minute);
+//                    morningSwitch.setChecked(true);
+//
+//
+//            }
+//        };
+//
+//    }
+//
+//    public void cancelAlarmIfExists(Context mContext,int requestCode,Intent intent){
+//        try {
+//            PendingIntent pendingIntent = PendingIntent.getBroadcast(mContext, requestCode, intent,0);
+//            AlarmManager am=(AlarmManager)mContext.getSystemService(Context.ALARM_SERVICE);
+//            am.cancel(pendingIntent);
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//    }
 }
+
 
